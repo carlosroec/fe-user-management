7ff4c5570a31ac1314e63eff1f80f6214fde593b
Carlos G. Rodriguez <carlosro.ec@gmail.com>
2020-01-27 13:31:51 -0500

    Home: table view added and data mapped

---
+++ src/components/FiltersHeader.vue
@@ -0,0 +1,14 @@
+<template>
+    <header>
+        <button class="button green">Add User</button>
+        <button class="button light-blue">Actions</button>
+        <button class="button blue">Filter Options</button>
+        <button class="button green">List Options</button>
+    </header>
+</template>
+
+<script>
+    export default {
+        name: 'FiltersHeader',
+    }
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,36 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <FiltersHeader />
+    <table-component
+      :data="[
+        { enabled: 'true', activated: 'true', name: 'Carlos', usergroup: 'A', email: 'a@a.com', lastVisit: '04/10/1940', registered: '04/10/1940', id: 10 },
+        { enabled: 'true', activated: 'true', name: 'Carlos', usergroup: 'A', email: 'a@a.com', lastVisit: '04/10/1940', registered: '04/10/1940', id: 10 },
+      ]"
+      sort-by="songs"
+      sort-order="asc"
+      >
+      <table-column show="enabled" label="Enabled"></table-column>
+      <table-column show="activated" label="Activated"></table-column>
+      <table-column show="name" label="Name"></table-column>
+      <table-column show="email" label="Email"></table-column>
+      <table-column show="usergroup" label="Usergroup"></table-column>
+      <table-column show="lastVisit" label="Last Visit"></table-column>
+      <table-column show="createdAt" label="Registered"></table-column>
+      <table-column show="id" label="ID"></table-column>
+    </table-component>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import FiltersHeader from '@/components/FiltersHeader.vue';
+import { TableComponent, TableColumn } from 'vue-table-component';
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    FiltersHeader,
+    TableComponent,
+    TableColumn
   }
 }
 </script>
